6cdda84b030dc075f2d6047c6a3fe0a44f300bb0
Francisco Nascimento <d19sp.developer@gmail.com>
2025-08-19 18:29:07 +0000

    Executing build feature/ADD_PR_CHECK

--- dist/reports/jcoco-report-parser.js
+++ dist/reports/jcoco-report-parser.js
@@ -1,4 +1,1 @@
-"use strict";
-// Deprecated: replaced by pluggable parsers in src/parsers/*.
-// Keeping the file to avoid breaking imports for users pinned to older versions.
-// This file can be removed in a future major release.
+export {};
